36acb40e6aded22d38b1d6d0de141d946ca663e1
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 21:09:51 +0530

    add Controllers to display avg rating

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -10,21 +10,20 @@ class RatingController extends Controller
 {
     public function display(string $id)
     {
-        // $rating = Rating::with(['blog:id,name', 'users:id,name'])->find($id);
         $rating = Rating::where('blog_id', $id)->with('users:id,name')->get();
 
         if (count($rating) == 0) {
             return response()->json([
                 "status" => false,
                 "message" => "Rating not found",
-            ]);
+            ], 404);
         }
 
         return response()->json([
             "status" => true,
             "message" => "Rating found successfully",
             "data" => $rating
-        ]);
+        ], 200);
     }
     public function store(RatingRequest $request)
     {
@@ -33,17 +32,28 @@ class RatingController extends Controller
             return response()->json([
                 "status" => false,
                 "message" => "Unable to create rating"
-            ]);
+            ], 500);
         }
 
         return response()->json([
             "status" => true,
             "message" => "Rating created successfully",
             "data" => $rating
-        ]);
+        ], 200);
     }
 
     public function displayavgRating(string $id)
     {
+        $avg = 0;
+        $rating = Rating::where('blog_id', $id)->with('users:id,name')->get();
+        foreach ($rating as $key => $value) {
+            $avg += $value->rating;
+        }
+        $avg = $avg / count($rating);
+        return response()->json([
+            "status" => true,
+            "message" => "Rating found successfully",
+            "data" => $avg
+        ], 200);
     }
 }
