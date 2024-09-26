41867967c602595a4e9a900c5fd5fc8d8169f4fa
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-26 16:48:11 +0530

    add migration for table rating

---
+++ app/Models/Migration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Migration extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'rating',
+        "blog_id",
+        "user_id",
+    ];
+
+    public function blog()
+    {
+        return $this->belongsTo(Blog::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
