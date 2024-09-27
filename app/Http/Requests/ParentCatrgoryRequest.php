381cf8c7031753a519d5dd55318ec0d775e793ef
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-27 12:22:37 +0530

    update the ParentCategoryRequest file to specify the image upload rule

--- app/Http/Requests/ParentCatrgoryRequest.php
+++ app/Http/Requests/ParentCatrgoryRequest.php
@@ -37,6 +37,7 @@ class ParentCatrgoryRequest extends FormRequest
     {
         return [
             "name" => "required|string|max:255",
+            "image" => "nullable|image|mimes:jpeg,png,jpg,gif|max:2048",
         ];
     }
 
@@ -46,6 +47,9 @@ class ParentCatrgoryRequest extends FormRequest
             'name.required' => 'Name is required',
             'name.string' => 'Name must be a string',
             'name.max' => 'Name is too long',
+            'image.mimes'=> 'image must be in form of jpeg,png,jpg,gif',
+            'image.max' => 'image is too large',
+            'image.image' => 'image must be an image',
         ];
     }
 }
