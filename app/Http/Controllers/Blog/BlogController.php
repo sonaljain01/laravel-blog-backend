ba91301ee9ee9eaf6596e2c0c63f74c9beb75501
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-07 21:09:28 +0530

    update the controller to handle softDeletes functionality

--- app/Http/Controllers/Blog/BlogController.php
+++ app/Http/Controllers/Blog/BlogController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Blog;
-use App\Models\Blog;
+
+use App\Http\Controllers\Controller;
 use App\Http\Requests\BlogStoreRequest;
 use App\Http\Requests\BlogUpdateRequest;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Blog;
 use Storage;
 use Str;
 
@@ -13,44 +13,45 @@ class BlogController extends Controller
 {
     public function display()
     {
-        $blogs = Blog::where("isdeleted", false)
-            ->where("type", "publish")
-            ->with(["users:id,name", "deletedBy:id,name", "parentCategory:id,name", "childCategory:id,name"])
+        $blogs = Blog::where('isdeleted', false)
+            ->where('type', 'publish')
+            ->with(['users:id,name', 'deletedBy:id,name', 'parentCategory:id,name', 'childCategory:id,name'])
             ->paginate(10);
 
         $returnData = [];
 
         foreach ($blogs as $blog) {
             $returnData[] = [
-                "id" => $blog->id,
-                "slug" => $blog->slug,
-                "title" => $blog->title,
-                "description" => $blog->description,
-                "photo" => $blog->photo,
-                "category" => $blog->parentCategory->name ?? "",
-                "sub_category" => $blog->childCategory->name ?? "",
-                "tag" => $blog->tag ?? "",
-                "created_at" => $blog->created_at,
-                "created_by" => $blog->users->name,
-                "is_deleted" => $blog->isdeleted,
-                "seo" => [
-                    "meta.name" => $blog->title,
-                    "meta.desc" => $blog->description,
-                    "meta.robots" => "noindex, nofollow"
-                ]
+                'id' => $blog->id,
+                'slug' => $blog->slug,
+                'title' => $blog->title,
+                'description' => $blog->description,
+                'photo' => $blog->photo,
+                'category' => $blog->parentCategory->name ?? '',
+                'sub_category' => $blog->childCategory->name ?? '',
+                'tag' => $blog->tag ?? '',
+                'created_at' => $blog->created_at,
+                'created_by' => $blog->users->name,
+                'is_deleted' => $blog->isdeleted,
+                'seo' => [
+                    'meta.name' => $blog->title,
+                    'meta.desc' => $blog->description,
+                    'meta.robots' => 'noindex, nofollow',
+                ],
 
             ];
         }
         $pagination = [
-            "next_page_url" => $blogs->nextPageUrl(),
-            "previous_page_url" => $blogs->previousPageUrl(),
-            "total" => $blogs->total(),
+            'next_page_url' => $blogs->nextPageUrl(),
+            'previous_page_url' => $blogs->previousPageUrl(),
+            'total' => $blogs->total(),
         ];
+
         return response()->json([
-            "status" => true,
-            "message" => "Blog fetched successfully",
-            "data" => $returnData,
-            "pagination" => $pagination
+            'status' => true,
+            'message' => 'Blog fetched successfully',
+            'data' => $returnData,
+            'pagination' => $pagination,
         ], 200);
 
     }
@@ -59,11 +60,11 @@ class BlogController extends Controller
     {
         $slug = null;
         if ($request->slug) {
-            $isBlogExist = Blog::where("slug", $request->slug)->first();
+            $isBlogExist = Blog::where('slug', $request->slug)->first();
             if ($isBlogExist) {
                 return response()->json([
-                    "status" => false,
-                    "message" => "Blog with this slug already exist",
+                    'status' => false,
+                    'message' => 'Blog with this slug already exist',
                 ]);
             } else {
                 $slug = $request->slug;
@@ -72,44 +73,46 @@ class BlogController extends Controller
             $slug = $this->slug($request->title);
         }
         $filldata = [
-            "user_id" => auth()->user()->id,
-            "title" => $request->title,
-            "description" => $request->description,
-            "photo" => $request->file('image') ? $this->uploadImage($request->file('image')) : null,
-            "parent_category" => $request->category,
-            "tag" => $request->tag,
-            "child_category" => $request->sub_category,
-            "slug" => $slug,
-            "type" => $request->type
+            'user_id' => auth()->user()->id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'photo' => $request->file('image') ? $this->uploadImage($request->file('image')) : null,
+            'parent_category' => $request->category,
+            'tag' => $request->tag,
+            'child_category' => $request->sub_category,
+            'slug' => $slug,
+            'type' => $request->type,
         ];
 
         $sendData = [
-            "subject" => "New blog created",
-            "title" => $request->title,
-            "description" => $request->description
+            'subject' => 'New blog created',
+            'title' => $request->title,
+            'description' => $request->description,
         ];
 
         $blog = Blog::create($filldata);
         // Http::post("https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTZkMDYzNTA0MzI1MjZlNTUzMDUxMzQi_pc", $sendData);
 
-        if ($request->type === "draft") {
+        if ($request->type === 'draft') {
             return response()->json([
-                "status" => true,
-                "message" => "Draft created successfully",
+                'status' => true,
+                'message' => 'Draft created successfully',
             ], 200);
         }
+
         return response()->json([
-            "status" => true,
-            "message" => "Blog created successfully",
-            "data" => $blog
+            'status' => true,
+            'message' => 'Blog created successfully',
+            'data' => $blog,
         ], 200);
     }
 
     public function update(BlogUpdateRequest $request, string $slug)
     {
         $blog = Blog::where('slug', $slug)->where('user_id', auth()->user()->id)->first();
-        if (!$blog) {
-            $this->error = "You are not allowed to update other person blog";
+        if (! $blog) {
+            $this->error = 'You are not allowed to update other person blog';
+
             return false;
         }
 
@@ -119,115 +122,167 @@ class BlogController extends Controller
             $filldata['photo'] = $this->uploadImage($request->file('image'));
         }
         $sendData = [
-            "subject" => "Blog with id." . $slug . " updated",
-            "title" => $request->title,
-            "description" => $request->description
+            'subject' => 'Blog with id.'.$slug.' updated',
+            'title' => $request->title,
+            'description' => $request->description,
         ];
 
         $isUpdate = $blog->update($filldata);
-        if (!$isUpdate) {
+        if (! $isUpdate) {
             return response()->json([
-                "status" => false,
-                "message" => "Unable to update blog",
+                'status' => false,
+                'message' => 'Unable to update blog',
             ]);
         }
 
         // Http::post("https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTZkMDYzNTA0MzI1MjZlNTUzMDUxMzQi_pc", $sendData);
         return response()->json([
-            "status" => true,
-            "message" => "Blog updated successfully",
-            "data" => $blog->fresh()
+            'status' => true,
+            'message' => 'Blog updated successfully',
+            'data' => $blog->fresh(),
         ]);
     }
 
     public function destroy(int $blog_id)
     {
-        if (!auth()->check()) {
+        if (! auth()->check()) {
             return response()->json([
-                "status" => false,
-                "message" => "Please login to delete blog",
+                'status' => false,
+                'message' => 'Please login to delete blog',
             ]);
         }
         $isBlogExist = Blog::find($blog_id);
 
-        if (!$isBlogExist) {
+        if (! $isBlogExist) {
             return response()->json([
-                "status" => false,
-                "message" => "Blog not found",
+                'status' => false,
+                'message' => 'Blog not found',
 
             ]);
         }
 
         if ($isBlogExist->isDeleted) {
             return response()->json([
-                "status" => false,
-                "message" => "Blog already deleted",
+                'status' => false,
+                'message' => 'Blog already deleted',
 
             ]);
         }
 
-        $isUpdate = $isBlogExist->update([
-            "isDeleted" => true,
-            "deleted_by" => auth()->user()->id
+        // $isUpdate = $isBlogExist->update([
+        //     "isDeleted" => true,
+        //     "deleted_by" => auth()->user()->id
+        // ]);
+        $isBlogExist->deleted_by = auth()->user()->id;
+        $isBlogExist->save();
+
+        // Soft delete the blog
+        $isBlogExist->delete();
+
+        // if ($isUpdate)
+        return response()->json([
+            'status' => true,
+            'message' => 'Blog deleted successfully',
+            'deletedBy' => 'Blog is deleted by You.',
         ]);
+    }
 
+    public function restore(int $blog_id)
+    {
+        $blog = Blog::withTrashed()->find($blog_id);
 
-        if ($isUpdate)
+        if (! $blog) {
             return response()->json([
-                "status" => true,
-                "message" => "Blog deleted successfully",
-                "deletedBy" => "Blog is deleted by You."
+                'status' => false,
+                'message' => 'Blog not found',
             ]);
+        }
+
+        if (! $blog->trashed()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Blog is not deleted',
+            ]);
+        }
+
+        $blog->restore();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Blog restored successfully',
+        ]);
+    }
+
+    public function forceDelete($id)
+    {
+        // Find the blog including soft deleted ones
+        $blog = Blog::withTrashed()->find($id);
+
+        if (! $blog) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Blog not found',
+            ], 404);
+        }
+
+        // Permanently delete the blog
+        $blog->forceDelete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Blog permanently deleted',
+        ], 200);
     }
 
     public function displayuserBlog()
     {
         //only authenticate user can see their blog
-        if (!auth()->check()) {
+        if (! auth()->check()) {
             return response()->json([
-                "status" => false,
-                "message" => "Please login first",
+                'status' => false,
+                'message' => 'Please login first',
             ], 401);
         }
         $id = auth()->user()->id;
-        $blogs = Blog::where("user_id", $id)
-            ->where("isdeleted", false)
-            ->with(["users:id,name", "deletedBy:id,name", "parentCategory:id,name", "childCategory:id,name"])
+        $blogs = Blog::where('user_id', $id)
+            ->where('isdeleted', false)
+            ->with(['users:id,name', 'deletedBy:id,name', 'parentCategory:id,name', 'childCategory:id,name'])
             ->paginate(20);
 
         $returnData = [];
 
         foreach ($blogs as $blog) {
             $returnData[] = [
-                "id" => $blog->id,
-                "slug" => $blog->slug,
-                "title" => $blog->title,
-                "description" => $blog->description,
-                "photo" => $blog->photo,
-                "category" => $blog->parentCategory->name ?? "",
-                "sub_category" => $blog->childCategory->name ?? "",
-                "tag" => $blog->tag ?? "",
-                "created_at" => $blog->created_at,
-                "created_by" => $blog->users->name,
-                "is_deleted" => $blog->isdeleted,
-                "type" => $blog->type,
-                "seo" => [
-                    "meta.name" => $blog->title,
-                    "meta.desc" => $blog->description,
-                    "meta.robots" => "noindex, nofollow"
-                ]
+                'id' => $blog->id,
+                'slug' => $blog->slug,
+                'title' => $blog->title,
+                'description' => $blog->description,
+                'photo' => $blog->photo,
+                'category' => $blog->parentCategory->name ?? '',
+                'sub_category' => $blog->childCategory->name ?? '',
+                'tag' => $blog->tag ?? '',
+                'created_at' => $blog->created_at,
+                'created_by' => $blog->users->name,
+                'is_deleted' => $blog->isdeleted,
+                'type' => $blog->type,
+                'seo' => [
+                    'meta.name' => $blog->title,
+                    'meta.desc' => $blog->description,
+                    'meta.robots' => 'noindex, nofollow',
+                ],
             ];
         }
         $pagination = [
-            "next_page_url" => $blogs->nextPageUrl(),
-            "previous_page_url" => $blogs->previousPageUrl(),
-            "total" => $blogs->total(),
+            'next_page_url' => $blogs->nextPageUrl(),
+            'previous_page_url' => $blogs->previousPageUrl(),
+            'total' => $blogs->total(),
         ];
+
         return response()->json([
-            "status" => true,
-            "message" => "Blog fetched successfully",
-            "data" => $returnData,
-            "pagination" => $pagination
+            'status' => true,
+            'message' => 'Blog fetched successfully',
+            'data' => $returnData,
+            'pagination' => $pagination,
         ], 200);
 
     }
@@ -244,43 +299,45 @@ class BlogController extends Controller
 
     public function displaySpecificBlog(string $slug)
     {
-        $blog = Blog::where("slug", $slug)
-            ->with(["users:id,name,email,type", "deletedBy:id,name", "parentCategory:id,name", "childCategory:id,name"])->first();
+        $blog = Blog::where('slug', $slug)
+            ->with(['users:id,name,email,type', 'deletedBy:id,name', 'parentCategory:id,name', 'childCategory:id,name'])->first();
 
-        if (!$blog) {
+        if (! $blog) {
             return response()->json([
-                "status" => false,
-                "message" => "Blog not found",
+                'status' => false,
+                'message' => 'Blog not found',
             ]);
         }
 
         if ($blog->draft) {
-            if (!auth()->check() || auth()->user()->id !== $blog->user_id) {
+            if (! auth()->check() || auth()->user()->id !== $blog->user_id) {
                 return response()->json([
-                    "status" => false,
-                    "message" => "Unauthorized to view this blog",
+                    'status' => false,
+                    'message' => 'Unauthorized to view this blog',
                 ]);
             }
         }
 
         return response()->json([
-            "status" => true,
-            "message" => "Blog fetched successfully",
-            "data" => $blog,
-            "seo" => [
-                "title" => $blog->title,
-                "description" => $blog->description,
-                "meta.robots" => "noindex, nofollow"
-            ]
+            'status' => true,
+            'message' => 'Blog fetched successfully',
+            'data' => $blog,
+            'seo' => [
+                'title' => $blog->title,
+                'description' => $blog->description,
+                'meta.robots' => 'noindex, nofollow',
+            ],
         ]);
     }
+
     protected function slug($title)
     {
         $slug = Str::slug($title);
-        $isBlogExist = Blog::where("slug", $slug)->first();
+        $isBlogExist = Blog::where('slug', $slug)->first();
         if ($isBlogExist) {
-            $slug = $slug . "-" . rand(1000, 9999);
+            $slug = $slug.'-'.rand(1000, 9999);
         }
+
         return $slug;
     }
 }
