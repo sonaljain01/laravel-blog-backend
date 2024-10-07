7ef13ddc3a3e465c90366e19050619b8cba14999
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-07 17:44:39 +0530

    update parent category controller

--- app/Models/ParentCategory.php
+++ app/Models/ParentCategory.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Translation\HasLocalePreference;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class ParentCategory extends Model
+
+class ParentCategory extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
+    public function registerMediaConversions(\Spatie\MediaLibrary\MediaCollections\Models\Media $media = null): void
+    {
+        $this->addMediaConversion('profile_images')
+             ->width(100)
+             ->height(100);
+    }
     protected $fillable = [
         'name',
         'image',
