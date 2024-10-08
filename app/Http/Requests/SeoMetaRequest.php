1d6498ed093ab69e00c57d4621d4aafa802868f5
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-08 15:59:22 +0530

    feat: create a request file for SeoMeta

---
+++ app/Http/Requests/SeoMetaRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SeoMetaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (auth()->check()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'meta_title' => 'nullable|string|max:255',
+            'meta_description' => 'nullable|string|max:255',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'meta_title.max' => 'Meta name is too long',
+            'meta_description.string' => 'Meta description must be a string',
+            'meta_description.max' => 'Meta description is too long',
+            'meta_title.string' => 'Meta name must be a string',
+        ];
+    }
+}
