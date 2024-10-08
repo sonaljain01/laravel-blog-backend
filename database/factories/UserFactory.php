e685e079fe8766e808c6c43d92e2f72b4b827a05
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-08 18:21:51 +0530

    feat: create databse factories to generate fake data

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // default password
+            'remember_token' => Str::random(20),
+        ];
+    }
+
+
+}
