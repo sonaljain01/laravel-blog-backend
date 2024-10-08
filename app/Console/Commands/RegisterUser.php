a24e70a222b9587e8c85df961345d5d03f11de76
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-08 19:23:00 +0530

    feat: create RegisterUser command file to write commands

---
+++ app/Console/Commands/RegisterUser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Hash;
+use App\Models\User;
+class RegisterUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:register {name} {email} {password} {type}';
+    protected $description = 'Register a new user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = Hash::make($this->argument('password'));
+        $type = $this->argument('type');
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'type' => $type,
+        ]);
+
+        if($user) {
+            $this->info("User registered successfully");
+        }else {
+            $this->error("Failed to register user");
+        }
+    }
+}
