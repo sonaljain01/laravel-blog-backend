da601cd255f158baf15ec7ebf20f46a6fe5c5361
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-09 14:32:13 +0530

    test: install all the dependencies for browser test

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     */
+    public function test_user_can_register(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/api/auth/register') // Change this to your actual registration page
+                    ->type('name', 'Test User') // The name of the input field
+                    ->type('email', 'testuser@example.com') // The name of the input field
+                    ->type('password', 'password') // The name of the input field
+                    ->type('password_confirmation', 'password') // The name of the input field
+                    ->type('type', 'user') // The name of the input field
+                    ->press('Register') // The text on the button
+                    ->waitForText('User registered successfully, Please verify Your Email', 10) // Wait for success message
+                    ->assertSee('User registered successfully, Please verify Your Email'); // Assert the message
+        });
+    }
+}
