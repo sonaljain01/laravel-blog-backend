f09bdae1223e48e6c72d34245d3485354da0ad63
sonaljain01 <sonaljain01@users.noreply.github.com>
2024-10-10 13:48:18 +0000

    Fixes coding style

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
-
 class Blog extends Model
 {
-    use HasFactory, SoftDeletes, Searchable;
+    use HasFactory, Searchable, SoftDeletes;
 
     protected $fillable = [
         'user_id',
