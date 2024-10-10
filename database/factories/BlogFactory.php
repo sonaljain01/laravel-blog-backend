0929cd80c2d61bf021017df36d11f9806979c77b
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-10 19:16:36 +0530

    feat: create blog seeders

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Blog;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BlogFactory extends Factory
+{
+    protected $model = Blog::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->name(),
+            'description' => fake()->text(),
+            'user_id' => 1,
+            'parent_category' => 1,
+            'child_category' => 1,
+            'tag' => 1,
+            'slug' => fake()->name(),
+            'type' => 'publish',
+            'photo' => "https://cdn-icons-png.flaticon.com/512/4123/4123763.png",
+        ];
+    }
+}
