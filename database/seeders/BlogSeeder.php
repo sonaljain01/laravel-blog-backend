884c5864e4148038825047f2cabb302f9302f143
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-14 18:29:02 +0530

    fix: update seeder to create dummy data

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -12,7 +12,7 @@ class BlogSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Blog::factory(10)->create();
+        \App\Models\Blog::factory(200)->create();
 
     }
 }
